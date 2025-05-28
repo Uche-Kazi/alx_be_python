439387c6145ed0b11453fae3ca695a5b5ac071b1
Thaddeus Onyeakazi <tonyeakazi23@gmail.com>
2025-05-28 20:59:28 +0100

    input_age_calculator

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,9 @@
+age = ...
+current_year = 2023
+target_year = 2050
+
+years_to_add = target_year - current_year
+
+future_age = age + years_to_add
+
+print("In 2050 you will be", future_age "years old")

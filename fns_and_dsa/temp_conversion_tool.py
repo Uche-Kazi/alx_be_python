ea14cf6e93688e38e8198f9c05c48d81fc8ce13d
Thaddeus Onyeakazi <tonyeakazi23@gmail.com>
2025-06-07 21:01:03 +0100

    up-to-date_temp_conversion_tool

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -4,8 +4,8 @@
 # These variables are defined at the top level of the script (global scope).
 # They are accessible for reading by all functions within this module without
 # needing special keywords.
-FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
-CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
 
 def convert_to_celsius(fahrenheit):
     """

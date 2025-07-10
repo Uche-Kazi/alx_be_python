ff8661bc46321d1e1517daa823110746ae9f4ebe
Thaddeus Onyeakazi <tonyeakazi23@gmail.com>
2025-07-10 16:33:19 +0100

    feat: Customize Book admin; Setup new LibraryProject environment

--- my_django_root/book_store/admin.py
+++ my_django_root/book_store/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Book # Import your Book model
 
-# Register your models here.
+# Define a custom ModelAdmin for the Book model
+class BookAdmin(admin.ModelAdmin):
+    # Fiels to display in the list view of the admin
+    list_display = ('title', 'author', 'published_date')
+    # Fields to enable search functionality on
+    search_fields = ('title', 'author')
+    # You can add more customizations here later, e.g., list_filter, fieldsets
+
+
+# Register the Book models with its custom ModelAdmin
+admin.site.register(Book, BookAdmin)
